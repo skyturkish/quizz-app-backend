db09fb74674c9b626b82f5511288d71637d91770
sky <gokturk.acr2002@gmail.com>
2022-12-03 01:29:20 +0300

    still learning ? still cofigure

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 
 const router = require('express').Router()
 
-router.get('/',(req,res) => { // request kullanıcıdan gelen istek 
-    // res.send('Hello world')// response kullanıcıya döneceğimiz cevap
-    //res.sendFile(`${__dirname}/index.html`) ---> html olduğu zaman böyle kullandık
-     res.render('index') // neden views  yazmadık çünkü default olarak views'ın altına bakıyor ve .pug yazmadık çünkü yukarıda app.set ile söyledik pug dosyası olduğunu
+router.get('/',(req,res) => { 
+     res.send('this is index page')
  })
 
  module.exports = router
