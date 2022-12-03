4b0f73150485d07d01ff5e9e99d31e35b5b7d98e
sky <gokturk.acr2002@gmail.com>
2022-12-04 01:01:39 +0300

    user can get and create

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,34 @@
 const router = require('express').Router()
 const userService = require('../services/user-service')
-const axios = require('axios').default;
 
 
 router.get('/', async(req,res)=> {
-    res.render('user')
+
+    const users = await userService.load()
+
+    res.send(users)
+
+    // res.render('user')
 })
+
+router.get('/:userId', async(req,res)=> {
+    const user = await userService.find(req.params.userId) 
+
+    if(!user) return res.status(404).send('Cannot find user!')
+
+    res.send(user)
+
+})
+
+router.get('/:userId/quiz', async(req,res)=> {
+    const user = await userService.find(req.params.userId) 
+
+    if(!user) return res.status(404).send('Cannot find user\nquiz!')
+
+    res.send(user.quizs)
+
+})
+
 
 router.post('/', async(req,res,next)=> { 
     try {
